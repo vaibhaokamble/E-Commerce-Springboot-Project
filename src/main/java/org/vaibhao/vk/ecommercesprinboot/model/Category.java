10c5d389a13ac88601fdad3d7689157e1576cec4
Vaibhao Kamble <vaibhaokamble@gmail.com>
2026-07-01 15:09:28 +0530

    lombok

--- src/main/java/org/vaibhao/vk/ecommercesprinboot/model/Category.java
+++ src/main/java/org/vaibhao/vk/ecommercesprinboot/model/Category.java
@@ -4,27 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long categoryId;
     private String categoryName;
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public Long getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
 }
